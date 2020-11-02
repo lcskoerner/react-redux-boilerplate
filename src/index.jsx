a8f73ab75002d991fdfb183a60dcb51e58ef879c
Lucas Koerner <lcskoerner@gmail.com>
2020-11-02 18:36:09 -0500

    initial commit

---
+++ src/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import reduxPromise from 'redux-promise';
+import logger from 'redux-logger';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { createHistory as history } from 'history';
+
+import '../assets/stylesheets/application.scss';
+
+const reducers = combineReducers({
+  // key: reducer
+});
+
+const middlewares = applyMiddleware(reduxPromise, logger);
+
+// render an instance of the component in the DOM
+ReactDOM.render(
+  <Provider store={createStore(reducers, {}, middlewares)}>
+    <Router history={history}>
+      <Switch>
+        TODO
+      </Switch>
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  mode: 'development',
+  devtool: 'source-map',
+  devServer: { historyApiFallback: true },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.s?css$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+};
